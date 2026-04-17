13c374fb026d8a68e13f94deb46ff916ac5b0dc3
w2ah <139755396+w2ah@users.noreply.github.com>
2026-04-17 12:24:10 +0200

    Update index.js

--- test/src/index.js
+++ test/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.min.css";
-import "../public/styles.css";
+import "../src/styles.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
